a44260c722b2422b09d08c783867fe3e656711da
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-05-29 16:36:32 +0100

    Create about page

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,20 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "/index.html",
+        browse: "/browse.html",
+        single: "/single.html",
+        404: "/404.html",
+        about: "/about.html",
+        contact: "/contact.html",
+        privacy: "/privacy.html",
+      },
+    },
+  },
 });
