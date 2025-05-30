472cc501ef32a62640564f1b0af023bb52fc348d
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-05-30 15:57:26 +0100

    Refactor code structure and add RTL support to all pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  root: "src",
+  publicDir: "../public",
   server: {
     port: 3000,
+    open: "/",
+    strictPort: true,
   },
   build: {
     rollupOptions: {
       input: {
-        main: "/index.html",
-        browse: "/browse.html",
-        single: "/single.html",
-        404: "/404.html",
-        about: "/about.html",
-        contact: "/contact.html",
-        privacy: "/privacy.html",
-        rtl: "/rtl.html",
+        main: "src/index.html",
+        browse: "src/browse.html",
+        single: "src/single.html",
+        404: "src/404.html",
+        about: "src/about.html",
+        contact: "src/contact.html",
+        privacy: "src/privacy.html",
+        rtl: "src/rtl.html",
       },
     },
+    outDir: "../dist",
+    emptyOutDir: true,
   },
 });
